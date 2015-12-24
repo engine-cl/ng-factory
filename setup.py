f52e0cf079671bc804356681a95915d43002f674
Jorge A. Medina <jorge@engine.cl>
2015-12-24 22:11:21 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Description
+__author__ = 'themanda'
+
+from distutils.core import setup
+
+setup(name='ng_factory',
+      version='1.0',
+      description='Factory ',
+      author='Jorge A. Medina',
+      author_email='jorge_at_engine_dot_cl',
+      url='http://github.com/mnothic/ng-factory',
+      packages=['factory'],
+      license='BSD')
