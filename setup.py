051eeb0f9fa1ceee1bc30a0c881a6058271c2ffc
Jorge A. Medina <jorge@engine.cl>
2015-12-25 22:47:29 +0100

    add setup.py struct package tree

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='ng_factory',
       description='Factory ',
       author='Jorge A. Medina',
       author_email='jorge_at_engine_dot_cl',
-      url='http://github.com/mnothic/ng-factory',
+      url='http://github.com/mnothic/factory_poc',
       packages=['ng_factory'],
       license='BSD')
