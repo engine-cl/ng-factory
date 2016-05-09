67c62c420241c94660290aa665f918349d755fe4
Jorge A. Medina <jorge@engine.cl>
2016-05-09 18:36:31 +0200

    change md to rst doc format markup

--- setup.py
+++ setup.py
@@ -15,10 +15,24 @@ except ImportError:
         """
         return ['ng_factory']
 
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+
 setup(
     name='ng_factory',
     version='1.0',
     description='Instrospective Factory pattern library',
+    long_description='\n\n'.join((read('README.rst'))),
+    classifiers=[
+        'License :: OSI Approved :: BSD Software License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Pattern :: Factory'],
     author='Jorge A. Medina',
     author_email='jorge@engine.cl',
     url='http://github.com/engine-cl/ng-factory',
