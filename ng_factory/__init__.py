5f7a47ba31c8897387b1d4de9a33a04b529a4723
Jorge A. Medina <jorge@engine.cl>
2016-05-26 10:45:30 +0200

    import namespace fixed

--- ng_factory/__init__.py
+++ ng_factory/__init__.py
@@ -1,5 +1,1 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Description
-__author__ = 'themanda'
+from .factory import factorize, ArgumentError, NonExistentTypeError, NonExistentModuleError
